c6f78787db4f08c37248c2f5a6823a9a43a491a7
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-21 14:22:56 -0700

    forgot to regularly commit

--- detail/detail.js
+++ detail/detail.js
@@ -7,3 +7,11 @@ const dogDetailContainer = document.getElementById('dog-detail-container');
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
+window.addEventListener('load', async() => {
+    const data = new URLSearchParams(window.location.search);
+    const dogId = data.get('id');
+
+    const dog = await getDog(dogId);
+    const dogEl = renderDogDetail(dog);
+    dogDetailContainer.append(dogEl);
+});
